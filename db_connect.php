d8f0f8808ec00a4c1973e8d5df55bb35cb084127
bobbymorante <164976758+bobbymorante@users.noreply.github.com>
2024-07-04 18:47:05 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','u155023598_kruxton','Kruxton123*','u155023598_kruxton')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','kruxton')or die("Could not connect to mysql".mysqli_error($con));
